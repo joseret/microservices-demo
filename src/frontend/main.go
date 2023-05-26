3672f1cf3aff686597c018754c7761f82059bd4a
Jose Retelny <joseret@google.com>
2023-05-26 14:54:47 -0700

    fix go stuff

--- src/frontend/main.go
+++ src/frontend/main.go
@@ -27,7 +27,6 @@ import (
 	"github.com/sirupsen/logrus"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-	"go.opentelemetry.io/contrib/propagators/b3"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/propagation"
@@ -101,7 +100,7 @@ func main() {
 
 	otel.SetTextMapPropagator(
 		propagation.NewCompositeTextMapPropagator(
-			propagation.TraceContext{}, propagation.Baggage{}, b3.New()))
+			propagation.TraceContext{}, propagation.Baggage{}))
 
 	if os.Getenv("ENABLE_TRACING") == "1" {
 		log.Info("Tracing enabled.")
